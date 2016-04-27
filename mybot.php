4d493949211b03be2673e429306b9ece4cefc2ba
kudymets <m.kudymets@gmail.com>
2016-04-28 00:01:13 +0300

    Create mybot.php

---
+++ mybot.php
@@ -0,0 +1,6 @@
+echo "# Test-my-bot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/kudymets/Test-my-bot.git
+git push -u origin master
