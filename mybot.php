cbb93988cacd3abb951cd45b8bc4cee7ef5eb631
kudymets <m.kudymets@gmail.com>
2016-04-28 00:20:20 +0300

    Delete mybot.php

--- mybot.php
+++
@@ -1,6 +0,0 @@
-echo "# Test-my-bot" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/kudymets/Test-my-bot.git
-git push -u origin master
